315caab4a9fdf71647cb981a41aae1efd158ffb7
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:22:40 -0500

    assignment one

---
+++ variables.py
@@ -0,0 +1,21 @@
+name="donald"
+age=1960
+variable=1.0
+
+print("next year i would be",age+variable)
+
+randomInt = 5
+randomFloat = 7.2
+randomBool = True
+randomNone = None
+randomString = "sup"
+
+print(type(randomInt))
+print(type(randomFloat))
+print(type(randomBool))
+print(type(randomNone))
+print(type(randomString))
+
+print("my name is," + name + ",and i am," + str(age))
+
+
